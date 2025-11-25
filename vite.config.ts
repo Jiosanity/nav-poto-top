c479141192b4cdd85f4cbd9d643c4e950263c384
小十 <jhm@alu.scu.edu.cn>
2025-11-25 16:10:11 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,16 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      // 新增：强制使用根路径，解决 index.css 404 问题
+      base: '/', 
+      
       server: {
         port: 3000,
         host: '0.0.0.0',
       },
       plugins: [react()],
       define: {
+        // 这里决定了我们需要在 GitHub 设置的变量名是 GEMINI_API_KEY
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
       },
